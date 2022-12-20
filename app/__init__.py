22344bc56ed57e2fad3efa8972969fce6a5a5120
Eva Liu <una229@gmail.com>
2022-12-20 00:40:41 -0500

    model set up and database connected

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    from .routes import book_bp
-    app.register_blueprint(book_bp) 
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+                                            
+    db.init_app(app)
+    migrate.init_app(app, db)    
+                                        
+    from app.models.book import Book
+    # app.register_blueprint(book_bp) 
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,55 +1,55 @@
-from flask import Blueprint, jsonify, abort, make_response
+# from flask import Blueprint, jsonify, abort, make_response
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
         
         
-books = [
-            Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-            Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-            Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-        ] 
+# books = [
+#             Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+#             Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+#             Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+#         ] 
 
 
-book_bp = Blueprint("books", __name__,  url_prefix="/books")
+# book_bp = Blueprint("books", __name__,  url_prefix="/books")
 
 
-@ book_bp.route("", methods = ["GET"])
-def book_lists():
-    books_response = []
-    for book in books:
-        books_response.append(
-            {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
-        )
-    return jsonify(books_response)
+# @ book_bp.route("", methods = ["GET"])
+# def book_lists():
+#     books_response = []
+#     for book in books:
+#         books_response.append(
+#             {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description
+#             }
+#         )
+#     return jsonify(books_response)
 
 
-def validate_book(book_id):
-    try:
-        book_id =int(book_id)
-    except:
-        abort(make_response({"msg" : f" book {book_id} invalid."}, 400))
+# def validate_book(book_id):
+#     try:
+#         book_id =int(book_id)
+#     except:
+#         abort(make_response({"msg" : f" book {book_id} invalid."}, 400))
     
-    for book in books:
-        if book.id == book_id:
-            return book
+#     for book in books:
+#         if book.id == book_id:
+#             return book
             
-    abort(make_response({"msg" : f" book {book_id} not found"}, 404))
+#     abort(make_response({"msg" : f" book {book_id} not found"}, 404))
     
 
 
-@ book_bp.route("/<book_id>", methods = ["GET"])
-def handle_book_by_id(book_id):
-    book = validate_book(book_id)
-    return { 
-        "id": book.id,
-        "title": book.title,
-        "author": book.description
-    }
+# @ book_bp.route("/<book_id>", methods = ["GET"])
+# def handle_book_by_id(book_id):
+#     book = validate_book(book_id)
+#     return { 
+#         "id": book.id,
+#         "title": book.title,
+#         "author": book.description
+#     }
