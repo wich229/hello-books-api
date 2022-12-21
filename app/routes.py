4b62e54d6d88a65f041d525e56be6f1063801343
Eva Liu <una229@gmail.com>
2022-12-20 22:09:14 -0500

    read

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,12 @@ from app import db
 from app.models.book import Book
 from flask import Blueprint, request, jsonify, abort, make_response
 
-#routes.py
-
 
 books_bp = Blueprint("books_bp", __name__,  url_prefix="/books")
 
-@books_bp.route("", methods = ["POST"])
-def handle_books():
+# create
+@ books_bp.route("", methods = ["POST"])
+def create_book():
     request_body = request.get_json()
     new_book = Book(
                     # id = request_body["id"],
@@ -21,6 +20,21 @@ def handle_books():
     
     return make_response(f"Book {new_book.title} successfully created", 201)
 
+
+@ books_bp.route("", methods = ["GET"])
+def handle_books():
+    books = Book.query.all() # will return a list of instances of books
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return jsonify(books_response)
+    
 
 # @ books_bp.route("", methods = ["GET"])
 # def book_lists():
